8fba0e3ce017ab2e34ea58d759eb70e019103395
CDLK <popoto38697@gmail.com>
2021-04-11 18:54:19 +0200

    Generation du site

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,21 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'blog',
+        data: { pageTitle: 'myblogApp.blog.home.title' },
+        loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule),
+      },
+      {
+        path: 'post',
+        data: { pageTitle: 'myblogApp.post.home.title' },
+        loadChildren: () => import('./post/post.module').then(m => m.PostModule),
+      },
+      {
+        path: 'tag',
+        data: { pageTitle: 'myblogApp.tag.home.title' },
+        loadChildren: () => import('./tag/tag.module').then(m => m.TagModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
